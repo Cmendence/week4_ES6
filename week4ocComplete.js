963f6b4b06e6d03bbf432bb711e5d8d81da093f0
Cmendence <chris.mendence@gmail.com>
2023-02-27 07:57:32 -0500

    added week4ocweek4ocComplete.js, week4research.pdf

---
+++ week4ocComplete.js
@@ -0,0 +1,104 @@
+// Monthly Payment:
+// Convert the following function myFunction into an arrow function. 
+// Refer to the new function as monthlyPayment.
+
+// function myFunction(yearlyPayment) {
+//     let monthly = yearlyPayment / 12;
+//     return monthly.toFixed(2);
+// }
+
+// const monthlyPayment = (yearlyPayment) => {
+//     return (yearlyPayment / 12).toFixed(2);
+// }
+
+// INSTRUCTOR SOLUTION
+// const monthlyPayment = (yearlyPayment) => {
+//     let monthly = yearlyPayment / 12;
+//     return monthly.toFixed(2);
+// }
+
+
+// A More Complex Function:
+// Convert the following function myFunction into an 
+// arrow function. Refer to the new function as fancyAlgorithm.
+
+// function myFunction(num1, num2, num3) {
+//     let value = 0;
+//     for (let i = 0; i < num3; i++){
+//         value = (value + num2) * num1;
+//     }
+
+//     return value / (num1 * (num3 * 10));
+// }
+
+// console.log(myFunction(4, 4, 4))
+
+
+// const fancyAlgorithm = (num1, num2, num3) => {
+//     let value = 0;
+//     for (let i = 0; i < num3; i++) {
+//       value = (value+=num2) * num1;
+//     }
+//     return value / (num1 * (num3 * 10));
+// }
+
+// console.log(fancyAlgorithm(4, 4, 4))
+
+// Template Literal:
+// Convert the following function myFunction into 
+// an arrow function. Refer to the new function as 
+// returnNumber.
+
+//  function myFunction(num) { return `Your number is ${num}`; }
+
+//  const returnNumber = (num) => `Your number is ${num}`;
+
+// Pythagorean theorem:
+// Convert the following function myFunction into 
+// an arrow function. Refer to the new function as pythagoras.
+
+// function myFunction(num1, num2) { return Math.sqrt((num1*num1)+(num2*num2)); }
+
+// const pythagoras = (num1, num2) => Math.sqrt((num1*num1)+(num2*num2));
+
+// Exclusive OR:
+// Convert the following function myFunction 
+// into an arrow function. Refer to the new function as xor.
+
+// function myFunction(num1, num2) {
+//     return num1 ^ num2;
+// }
+
+// console.log(myFunction(1,5));
+
+
+// const xor = (num1, num2) => num1 ^ num2;
+
+// console.log(xor(1,5))
+
+
+// Square Root:
+// Convert the following function myFunction into an 
+// arrow function. Refer to the new function as returnSquareRoot.
+
+// function myFunction(num) { return Math.sqrt(num); }
+
+
+// Addition:
+// Convert the following function myFunction into an 
+// arrow function. Refer to the new function as returnSum.
+
+// function myFunction(a,b) { return a+b; }
+
+// const returnSum = (a,b) => a + b;
+
+// Revert:
+// Convert the following function myFunction into 
+// an arrow function. Refer to the new function as revert.
+
+// function myFunction(value){ return !value; }
+
+
+// const revert = (value) => !value;
+
+
